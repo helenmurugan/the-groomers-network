fc3b0498b2def097216446d082eced9410a6a0ac
Helen Murugan <gallonhelen@hotmail.com>
2023-11-05 21:35:36 +0000

    feat:Add view code for Post and Event

--- event/views.py
+++ event/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Event
 
-# Create your views here.
+class PostList(generic.ListView):
+    model = Event
+    queryset = Post.objects.order_by("-created_on")
+    template_name = "event.html"
+    paginate_by = 6
+

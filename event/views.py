0c9e7374a7c28188edbd4966f79c39d507d73c62
Helen Murugan <gallonhelen@hotmail.com>
2023-11-09 21:09:49 +0000

    feat:Add EventDetail View and wire up page links

--- event/views.py
+++ event/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Event
 
 class EventList(generic.ListView):
@@ -8,3 +8,23 @@ class EventList(generic.ListView):
     template_name = "event.html"
     paginate_by = 6
 
+
+class EventDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Event.objects
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "event_detail.html",
+            {
+                "event": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )

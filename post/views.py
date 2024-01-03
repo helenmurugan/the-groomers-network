2d8c3a963dfd78d05d38c8566d2fcd4575b6c8b0
Helen Murugan <gallonhelen@hotmail.com>
2024-01-03 21:27:10 +0000

    style: Reverse the order of the comments and style buttons and links

--- post/views.py
+++ post/views.py
@@ -32,7 +32,7 @@ class PostDetail(View):
         """
         queryset = Post.objects
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by("-created_on")
+        comments = post.comments.filter(approved=True).order_by("created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
@@ -54,7 +54,7 @@ class PostDetail(View):
         """
         queryset = Post.objects
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.all().order_by("-created_on")
+        comments = post.comments.all().order_by("created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True

4fb820c47c67c319bd5450ded42bd3d74f98888a
Helen Murugan <gallonhelen@hotmail.com>
2024-01-13 19:35:29 +0000

    fix: validate and fix python code

--- manage.py
+++ manage.py
@@ -5,8 +5,13 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'groomers_network.settings')
+    """
+    Run administrative tasks
+    """
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE',
+        'groomers_network.settings'
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
